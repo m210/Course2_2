dc8bc7f7962e576a8b42268a93607996ca25e7c5
[M210®] <m210-2007@mail.ru>
2022-01-18 19:01:42 +0300

    Fixes

--- src/main/java/com/example/lesson2/CalculatorController.java
+++ src/main/java/com/example/lesson2/CalculatorController.java
@@ -14,23 +14,30 @@ public class CalculatorController {
         this.service = service;
     }
 
+    @GetMapping
+    public String hello() {
+        return "Добро пожаловать в калькулятор";
+    }
+
     @GetMapping("/plus")
-    public String plus(@RequestParam(required = true) int num1, @RequestParam(required = true) int num2) {
+    public String plus(@RequestParam int num1, @RequestParam int num2) {
         return num1 + " + " + num2 + " = " + service.plus(num1, num2);
     }
 
     @GetMapping("/minus")
-    public String minus(@RequestParam(required = true) int num1, @RequestParam(required = true) int num2) {
+    public String minus(@RequestParam int num1, @RequestParam int num2) {
         return num1 + " - " + num2 + " = " + service.minus(num1, num2);
     }
 
     @GetMapping("/multiply")
-    public String multiply(@RequestParam(required = true) int num1, @RequestParam(required = true) int num2) {
+    public String multiply(@RequestParam int num1, @RequestParam int num2) {
         return num1 + " * " + num2 + " = " + service.multiply(num1, num2);
     }
 
     @GetMapping("/devide")
-    public String devide(@RequestParam(required = true) int num1, @RequestParam(required = true) int num2) {
+    public String devide(@RequestParam int num1, @RequestParam int num2) {
+        if(num2 == 0)
+            return num1 + " shouldn't be divided to 0";
         return num1 + " / " + num2 + " = " + service.devide(num1, num2);
     }
 }
