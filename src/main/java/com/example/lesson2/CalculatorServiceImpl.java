e4ed005453511c93d1be46c0cf1911576dedd92e
[M210®] <m210-2007@mail.ru>
2022-02-23 20:59:21 +0300

    Lesson_2.12 Homework

--- src/main/java/com/example/lesson2/CalculatorServiceImpl.java
+++ src/main/java/com/example/lesson2/CalculatorServiceImpl.java
@@ -22,8 +22,9 @@ public class CalculatorServiceImpl implements CalculatorService {
 
     @Override
     public int devide(int num1, int num2) {
-        if(num2 == 0)
+        if(num2 == 0) {
             throw new IllegalArgumentException(num1 + " shouldn't be divided by 0");
+        }
         return num1 / num2;
     }
 }
